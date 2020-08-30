08c4ec482f956908b46a6929836a46c53f0d713c
arslanbekova <lyuda.arslanbekova@gmail.com>
2020-08-30 18:43:27 +0300

    created presentor

--- src/main.js
+++ src/main.js
@@ -1,22 +1,13 @@
 import UserRank from "./view/user-rank.js";
 import SiteMenu from "./view/site-menu.js";
-import SortList from "./view/sort-list.js";
-import ContentSection from "./view/content-section.js";
-import FilmCard from "./view/film-card.js";
-import ShowMoreButton from "./view/show-more-button.js";
+import FilmsList from "./presenter/films-list.js"
 import FooterStatistic from "./view/footer-statistic.js";
-import FilmPopup from "./view/film-popup.js";
-import noFilms from "./view/no-films.js"
-import {render, remove} from "./utils/render.js";
-import {isEscEvent} from "./utils/general.js";
+
+import {render} from "./utils/render.js";
+import {NUMBER_ALL_FILMS_RENDERED_CARDS} from "./utils/consts.js";
 import {generateFilm} from "./mock/film.js";
 import {generateUserRank} from "./mock/user-rank.js";
 import {generateSiteMenuFilters} from "./mock/site-menu.js";
-
-const NUMBER_ALL_FILMS_RENDERED_CARDS = 20;
-const MAX_NUMBER_ALL_FILMS_RENDERED_CARDS = 5;
-const ALL_FILMS_RENDERED_CARDS_PER_STEP = 5;
-const MAX_NUMBER_EXTRA_FILMS_RENDERED_CARDS = 2;
 
 const films = new Array(NUMBER_ALL_FILMS_RENDERED_CARDS).fill().map(generateFilm);
 const filters = generateSiteMenuFilters(films);
@@ -27,76 +18,9 @@ const siteFooterElement = document.querySelector(`.footer`);
 
 render(siteHeaderElement, new UserRank(generateUserRank(filters)));
 render(siteMainElement, new SiteMenu(filters));
-render(siteMainElement, new SortList());
-render(siteMainElement, new ContentSection());
 
-const filmsElement = siteMainElement.querySelector(`.films`);
-const allFilmsListElement = filmsElement.querySelector(`.films-list`);
-const allFilmsCardsContainerElement = allFilmsListElement.querySelector(`.films-list__container`);
-
-const renderFilmCard = (container, film) => {
-  const filmCardComponent = new FilmCard(film);
-  const filmPopupComponent = new FilmPopup(film);
-
-  const onPopupCloseBtnEscPress = (evt) => {
-    isEscEvent(evt, popupClose);
-  };
-
-  const popupOpen = () => {
-    render(container, filmPopupComponent);
-    document.addEventListener(`keydown`, onPopupCloseBtnEscPress);
-    filmPopupComponent.setOnPopupCloseBtnClick(popupClose);
-  };
-
-  const popupClose = () => {
-    remove(filmPopupComponent);
-    document.removeEventListener(`keydown`, onPopupCloseBtnEscPress);
-  }
-
-  filmCardComponent.setOnFilmCardElementClick(popupOpen);
-  render(container, filmCardComponent);
-};
-
-const extraFilmsListElements = filmsElement.querySelectorAll(`.films-list--extra`);
-const [topRatedFilmsListElement, mostCommentedFilmsListElement] = extraFilmsListElements;
-const topRatedFilmsCardsContainerElement = topRatedFilmsListElement.querySelector(`.films-list__container`);
-const mostCommentedFilmsCardsContainerElement = mostCommentedFilmsListElement.querySelector(`.films-list__container`);
-
-if (films.length === 0) {
-  allFilmsCardsContainerElement.remove();
-  extraFilmsListElements.forEach(element => element.remove());
-  render(allFilmsListElement, new noFilms());
-} else {
-  for (let film of films.slice(0, MAX_NUMBER_ALL_FILMS_RENDERED_CARDS)) {
-    renderFilmCard(allFilmsCardsContainerElement, film);
-  }
-};
-
-if (films.length > ALL_FILMS_RENDERED_CARDS_PER_STEP) {
-  let renderedFilms = ALL_FILMS_RENDERED_CARDS_PER_STEP;
-
-  const showMoreButtonComponent = new ShowMoreButton()
-  render(allFilmsListElement, showMoreButtonComponent);
-
-  const showMoreFilms = () => {
-    films
-      .slice(renderedFilms, renderedFilms + ALL_FILMS_RENDERED_CARDS_PER_STEP)
-      .forEach((film) => renderFilmCard(allFilmsCardsContainerElement, film));
-
-    renderedFilms += ALL_FILMS_RENDERED_CARDS_PER_STEP;
-
-    if (renderedFilms >= films.length) {
-      remove(showMoreButtonComponent);
-    }
-  }
-
-  showMoreButtonComponent.setOnShowMoreButtonClick(showMoreFilms)
-};
-
-for (let film of films.slice(0, MAX_NUMBER_EXTRA_FILMS_RENDERED_CARDS)) {
-  renderFilmCard(topRatedFilmsCardsContainerElement, film);
-  renderFilmCard(mostCommentedFilmsCardsContainerElement, film);
-};
+const filmsList = new FilmsList(siteMainElement);
+filmsList.init(films);
 
 const [, watched] = filters;
 render(siteFooterElement, new FooterStatistic(watched.count));
--- src/view/no-films.js
+++ src/view/no-films.js
@@ -2,11 +2,15 @@ import Abstract from "./abstract.js";
 
 const createNoFilmsTenplate = () => {
   return (
-    `<h2 class="films-list__title">There are no movies in our database</h2>`
+    `<section class="films">
+      <section class="films-list">
+        <h2 class="films-list__title">There are no movies in our database</h2>
+      </section>
+    </section`
   );
 };
 
-export default class noFilms extends Abstract {
+export default class NoFilms extends Abstract {
 
   getTemplate() {
     return createNoFilmsTenplate();
